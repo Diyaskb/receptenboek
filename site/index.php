486889be52989b0ac06e133f44af9c7a2700b29c
Diya Bassi <dskbassi@gmail.com>
2023-04-02 00:51:47 +0200

    eindproject blok 7

--- site/index.php
+++ site/index.php
@@ -1,5 +1,5 @@
 <?php
-
+// Rol 1 = Gebruiker // Rol 2 = Beheerder
 // Dit is het startpunt van je applicatie.
 
-echo 'Welkom bij het receptenboek';
+echo 'Welkom bij het receptenboek Wok wonderen';
